a1ef9017e2814f6c8b4ffc7df4eec0a8f2ec92fa
Muhammad Arif <123452078+F22BSEEN1E02102@users.noreply.github.com>
2023-02-24 11:04:59 -0700

    Add files via upload

---
+++ project_opp.py
@@ -0,0 +1,19 @@
+class campuse():
+        name="Name"
+        depart="software Engeenring"
+        total_room=2313
+        section="E2"
+        campuse="Baghdad campuse"
+    # def __init__(self,department,room):
+    #     self.department=department
+    #     self.room=room
+        
+        def calling(self):
+            print(f"{self.name} deportment of {self.depart} my depart has rooms {self.total_room} and I am study in the {self.campuse}")
+a=campuse()
+a.name='Muhammad Arif'
+# a.rooms=150
+# a.section="E2"
+# a.students=233331
+
+a.calling()
